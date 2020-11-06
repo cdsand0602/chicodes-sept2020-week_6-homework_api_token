caae67d10f42120c4d04692bb9efc3d5a09355d9
Crystal Sanders <crystalsanders@Crystals-MacBook-Pro.local>
2020-11-05 22:09:44 -0600

    first commit

---
+++ faker_seed_db.py
@@ -0,0 +1,23 @@
+from faker import Faker
+
+# Creation of faker profile helper function
+def getProfile():
+    fake = Faker()
+    return fake.profile()
+    
+# Gather Data and place inside of database
+import os
+from flask_api.models import Employee
+from flask_api import db
+
+def seedData():
+    for seed_num in range(10):
+        data = getProfile()
+        employee = Employee(data['name'],\
+        data['sex'],data['address'], data['ssn'],data['blood_group'],data['mail'])
+        
+        db.session.add(employee)
+        db.session.commit()
+
+
+seedData()
